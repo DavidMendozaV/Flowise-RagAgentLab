bbfa7d252c9a0d8510464e8725ae7ef6cc91b728
David <davidalejandromenver@gmail.com>
2025-06-18 09:40:40 -0400

    Update - Renaming files

---
+++ app/Embed/Fullpage-React.jsx
@@ -0,0 +1,10 @@
+import { FullPageChat } from "flowise-embed-react"
+
+const App = () => {
+    return (
+        <FullPageChat
+            chatflowid="c233ebd1-535d-4737-879b-10e5cddade95"
+            apiHost="http://localhost:3000"
+        />
+    );
+};
